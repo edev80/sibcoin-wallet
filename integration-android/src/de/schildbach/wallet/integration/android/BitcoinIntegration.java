2459082d1ab349037a17819b70efe8794bb4f3dd
edev80 <edev80@users.noreply.github.com>
2016-06-21 18:07:52 +0200

    fix scheme and app installation details

--- integration-android/src/de/schildbach/wallet/integration/android/BitcoinIntegration.java
+++ integration-android/src/de/schildbach/wallet/integration/android/BitcoinIntegration.java
@@ -32,7 +32,7 @@ public final class BitcoinIntegration
 	private static final String INTENT_EXTRA_PAYMENT = "payment";
 	private static final String INTENT_EXTRA_TRANSACTION_HASH = "transaction_hash";
 
-	private static final String MIMETYPE_PAYMENTREQUEST = "application/digitalcoin-paymentrequest"; // BIP 71
+	private static final String MIMETYPE_PAYMENTREQUEST = "application/sibcoin-paymentrequest"; // BIP 71
 
 	/**
 	 * Request any amount of Bitcoins (probably a donation) from user, without feedback from the app.
@@ -229,7 +229,7 @@ public final class BitcoinIntegration
 
 	private static Intent makeBitcoinUriIntent(final String address, final Long amount)
 	{
-		final StringBuilder uri = new StringBuilder("digitalcoin:");
+		final StringBuilder uri = new StringBuilder("sibcoin:");
 		if (address != null)
 			uri.append(address);
 		if (amount != null)
@@ -269,10 +269,10 @@ public final class BitcoinIntegration
 
 	private static void redirectToDownload(final Context context)
 	{
-		Toast.makeText(context, "No digitalcoin application found.\nPlease install digitalcoin Wallet.", Toast.LENGTH_LONG).show();
+		Toast.makeText(context, "No sibcoin application found.\nPlease install sibcoin Wallet.", Toast.LENGTH_LONG).show();
 
-		final Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=hashengineering.digitalcoin.wallet"));
-		final Intent binaryIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/HashEngineering/digitalcoin-wallet/releases"));
+		final Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=rusapps.sibcoin.wallet"));
+		final Intent binaryIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/edev80/sibcoin-wallet/releases"));
 
 
 		final PackageManager pm = context.getPackageManager();
